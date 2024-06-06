b286d18d4408fcd41013c656e3567a2f86ef6a12
hieumn <cntt.tts1@anphatholdings.com>
2024-06-06 23:17:51 +0700

    upload code

--- LineCentury/src/pages/OHLCChart/OHCLChart.tsx
+++ LineCentury/src/pages/OHLCChart/OHCLChart.tsx
@@ -1,3 +1,29 @@
+import { useState } from 'react'
+import * as Papa from 'papaparse'
+
 export default function OHCLChart() {
-  return <div>OHCLChart</div>
+  const [data, setData] = useState<object>([])
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+
+    if (file) {
+      Papa.parse(file, {
+        header: true,
+        complete: (results) => {
+          setData(results.data)
+        },
+        error: (error) => {
+          console.error('Error parsing CSV:', error)
+        },
+      })
+    }
+  }
+
+  console.log(data)
+  return (
+    <div className="max-w-7xl mx-auto p-4">
+      <input type="file" accept=".xlsx, .xls, .csv" onChange={handleFileUpload} />
+    </div>
+  )
 }
